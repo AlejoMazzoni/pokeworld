5e54237a016974802b2437abb811ae83f1c8febe
Alejo <mazzoni.alejo@gmail.com>
2023-08-15 17:21:29 -0300

    FEAT: Fixed styles

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,12 +1,8 @@
 import styles from "./Landing.module.scss";
-import pokeworldLogo from "../../assets/images/pokeworld-logo.png";
 
 const Landing = () => {
   return (
     <div className={styles.landingContainer}>
-      <div className={styles.logoContainer}>
-        <img src={pokeworldLogo} />
-      </div>
       <div className={styles.infoContainer}>
         <h1>Title</h1>
         <p>
