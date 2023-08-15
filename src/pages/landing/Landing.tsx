1de50ea3fda8dc1cb1bb2cffe4d27e3264d19512
Alejo <mazzoni.alejo@gmail.com>
2023-08-15 16:12:35 -0300

    REFACTOR: Added logo and banner to landing page

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,9 +1,23 @@
 import styles from "./Landing.module.scss";
-import Header from "../../components/ui/Header/Header";
-import Footer from "../../components/ui/Footer/Footer";
+import pokeworldLogo from "../../assets/images/pokeworld-logo.png";
 
 const Landing = () => {
-  return <div className={styles.landingContainer}></div>;
+  return (
+    <div className={styles.landingContainer}>
+      <div className={styles.logoContainer}>
+        <img src={pokeworldLogo} />
+      </div>
+      <div className={styles.infoContainer}>
+        <h1>Title</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem fugiat
+          ratione explicabo, fugit iusto unde. Deleniti ducimus laboriosam culpa
+          autem soluta! Temporibus minima corrupti ea porro officia dignissimos
+          sunt dolorum.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
