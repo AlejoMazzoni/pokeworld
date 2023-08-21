e3b70a2732d155ab5236e59f611c471e1cc9d5d3
Alejo <mazzoni.alejo@gmail.com>
2023-08-21 13:04:21 -0300

    FEAT: Implemented hash mode in router

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,18 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
-  <BrowserRouter basename="/pokeworld">
+  <HashRouter
+    basename={`${process.env.NODE_ENV === "production" ? "/pokeworld/" : "/"}`}
+  >
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
