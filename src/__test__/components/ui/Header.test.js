e32c763da829f14e3149ad7b332347082cb608ea
Alejo <mazzoni.alejo@gmail.com>
2023-08-22 16:04:29 -0300

    TEST: Added header test

---
+++ src/__test__/components/ui/Header.test.js
@@ -0,0 +1,10 @@
+import renderer from "react-test-renderer";
+import Header from "../../../components/ui/Header/Header";
+
+describe("Header", () => {
+  it("renders everything correctly", () => {
+    const component = renderer.create(<Header />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
