37c690cd784b07c7fe9fa08181709b45606cc9f7
Alejo <mazzoni.alejo@gmail.com>
2023-08-28 17:38:12 -0300

    REFACTOR: darkmode button in header

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,3 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Footer from "../components/ui/Footer/Footer";
 import Header from "../components/ui/Header/Header";
 import NotFound from "../pages/404/NotFound";
@@ -6,8 +5,6 @@ import Landing from "../pages/landing/Landing";
 import styles from "./App.module.scss";
 import { Routes, Route } from "react-router-dom";
 import useLocalStorage from "use-local-storage";
-import { faMoon as faMoonSolid } from "@fortawesome/free-solid-svg-icons";
-import { faMoon as faMoonRegular } from "@fortawesome/free-regular-svg-icons";
 
 function App() {
   const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
@@ -24,22 +21,7 @@ function App() {
   return (
     <div className={styles.appContainer} data-theme={theme}>
       <div className={styles.headerTopContainer}>
-        <Header />
-        <button onClick={switchTheme}>
-          {theme === "dark" ? (
-            <FontAwesomeIcon
-              icon={faMoonSolid}
-              size="2x"
-              className={styles.darkmodeIcon}
-            />
-          ) : (
-            <FontAwesomeIcon
-              icon={faMoonRegular}
-              size="2x"
-              className={styles.darkmodeIcon}
-            />
-          )}
-        </button>
+        <Header switchTheme={switchTheme} theme={theme} />
       </div>
       <div className={styles.mainPageTopContainer}>
         <Routes>
--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -1,11 +1,35 @@
 import styles from "./Header.module.scss";
 import logoHeader from "../../../assets/images/logo-header.png";
 
-const Header = () => {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon as faMoonSolid } from "@fortawesome/free-solid-svg-icons";
+import { faMoon as faMoonRegular } from "@fortawesome/free-regular-svg-icons";
+
+interface IHeader {
+  switchTheme: Function;
+  theme: String;
+}
+
+const Header = ({ switchTheme, theme }: IHeader) => {
   return (
     <div className={styles.headerContainer}>
       <div className={styles.logoContainer}>
         <img src={logoHeader} alt="Pokeball logo" />
+        <button onClick={(event) => switchTheme()}>
+          {theme === "dark" ? (
+            <FontAwesomeIcon
+              icon={faMoonSolid}
+              size="2x"
+              className={styles.darkmodeIcon}
+            />
+          ) : (
+            <FontAwesomeIcon
+              icon={faMoonRegular}
+              size="2x"
+              className={styles.darkmodeIcon}
+            />
+          )}
+        </button>
       </div>
     </div>
   );
